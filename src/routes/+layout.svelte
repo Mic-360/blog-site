9ec30cbfebc3c471d7bfcd1a0320a9a59a5b337c
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-07-22 01:44:19 +0530

    chore: Update favicon layout and body class with their deps
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Ripple from '$lib/ripple.svelte';
+	import Header from './Header.svelte';
+	import Footer from './Footer.svelte';
+</script>
+
+<div class="layout relative overflow-hidden bg-background md:shadow-xl rounded-lg">
+	<Header />
+	<main>
+		<slot />
+
+		<Ripple />
+	</main>
+	<Footer />
+</div>
+
+<style lang="postcss">
+	.layout {
+		display: grid;
+		grid-template-rows: auto 1fr auto;
+		min-height: 100vh;
+	}
+	main {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: center;
+	}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,25 @@
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		extend: {}
+		extend: {
+			fontFamily: {
+				sans: 'var(--font- sans)',
+				mono: 'var(--font-mono)'
+			},
+			animation: {
+				ripple: 'ripple var(--duration,2s) ease calc(var(--i, 0)*.2s) infinite'
+			},
+			keyframes: {
+				ripple: {
+					'0%, 100%': {
+						transform: 'translate(-50%, -50%) scale(1)'
+					},
+					'50%': {
+						transform: 'translate(-50%, -50%) scale(0.9)'
+					}
+				}
+			}
+		}
 	},
 	plugins: []
 };
