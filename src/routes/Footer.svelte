1f8472003d3c413226ecd662a613f7e0b12d580f
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-07-22 01:43:13 +0530

    feat: Add Footer and Header components
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

---
+++ src/routes/Footer.svelte
@@ -0,0 +1,67 @@
+<script lang="ts">
+	import { author, socials, about } from '$lib/config';
+	import { Facebook, House, Send, Instagram, Mail, Linkedin, Github } from 'lucide-svelte';
+</script>
+
+<footer>
+	<h1 class="text-sm font-semibold px-4 uppercase">About {author.split(' ')[0]}</h1>
+	<div class="flex px-4 py-4 gap-x-6">
+		<div class="relative">
+			<div class="absolute inset-0 blur-sm bg-slate-800 opacity-10 rounded-full"></div>
+			<img src="./avatar.png" alt="logo" class="h-28" />
+		</div>
+		<div class="max-w-lg space-y-4">
+			<p class="text-justify">
+				{about}
+			</p>
+			<ul class="flex gap-4">
+				<li>
+					<a href={socials.website}>
+						<House />
+					</a>
+				</li>
+				<li>
+					<a href={socials.github}>
+						<Github />
+					</a>
+				</li>
+				<li>
+					<a href={socials.linkedin}>
+						<Linkedin />
+					</a>
+				</li>
+				<li>
+					<a href={socials.mail}>
+						<Mail />
+					</a>
+				</li>
+				<li>
+					<a href={socials.instagram}>
+						<Instagram />
+					</a>
+				</li>
+				<li>
+					<a href={socials.facebook}>
+						<Facebook />
+					</a>
+				</li>
+				<li>
+					<a href={socials.telegram}>
+						<Send />
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+</footer>
+
+<style>
+	footer {
+		display: flex;
+		flex-direction: column;
+		padding: 1rem;
+		gap: 0.5rem;
+		background-color: hsl(var(--foreground));
+		color: hsl(var(--background));
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import { title, description, version, author, siteUrl } from '$lib/config';
+</script>
+
+<header>
+	<div class="relative">
+		<div class="absolute inset-0 blur-lg bg-blue-300 opacity-10 rounded-full"></div>
+		<img src="./avatar.png" alt="logo" class="h-28" />
+	</div>
+	<h1 class="text-5xl font-semibold uppercase">{title}</h1>
+	<span class="text-right w-screen max-w-md"
+		>-{' '}<a href={siteUrl} class="cursor-pointer hover:underline underline-offset-2">
+			{author}
+		</a></span
+	>
+	<h2 class="max-w-screen-sm text-center">{description}</h2>
+	<h3 class="text-right w-screen max-w-md font-bold">Version: {version}</h3>
+</header>
+
+<style>
+	header {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		padding: 0.5rem;
+		gap: 0.5rem;
+	}
+</style>
