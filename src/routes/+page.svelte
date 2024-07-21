b0e519d286f8771b512fe965bafd49936be74d50
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-07-21 14:36:00 +0530

    feat: Add tailwindcss for styling
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import '../app.css';
+</script>
+
+<section class="flex flex-col gap-4 w-full h-screen justify-center items-center">
+	<h1 class="text-5xl">Welcome to <span class="font-bold text-orange-400">Svelte</span>Kit</h1>
+	<p>
+		Visit <a
+			class="underline underline-offset-2 decoration-slate-400 hover:text-blue-400 hover:decoration-blue-400"
+			href="https://kit.svelte.dev">kit.svelte.dev</a
+		> to read the documentation.
+	</p>
+</section>
