e945ff6fd6cf6bd0652ea5ab5d132f513064232d
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-07-23 21:39:41 +0530

    chore: Update mdsvex.config.js to use @bitmachina/highlighter for syntax highlighting and remove tailwindcss
    Signed-off-by: Bhaumik Singh <Bhaumiksingh2000@gmail.com>

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,7 +1,11 @@
+import { createHighlighter } from '@bitmachina/highlighter';
+
 /** @type {import('mdsvex').MdsvexOptions} */
 const MdsvexOptions = {
 	extensions: ['.svelte.md', '.md', '.svx'],
-
+	highlight: {
+		highlighter: await createHighlighter({ theme: 'css-variables' })
+	},
 	smartypants: {
 		dashes: 'oldschool'
 	},
