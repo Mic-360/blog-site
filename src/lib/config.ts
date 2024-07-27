db9a2ae7d3b556cf3047b28d5a215817f87ee207
Bhaumik Singh <Bhaumiksingh2000@gmail.com>
2024-07-28 03:38:54 +0530

    Release v0.0.3

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -4,7 +4,7 @@ export const title = 'No-Nonsense Blogs';
 export const author = 'Bhaumic Singh';
 export const description =
 	'This is a blog site built with SvelteKit. Which covers the adventures of my developer journey. It is a place where I share my thoughts, ideas, and experiences.';
-export const version = '0.0.2';
+export const version = '0.0.3';
 export const url = dev ? 'http://localhost:5173' : 'https://blog-bhaumics-projects.vercel.app';
 export const apiUrl = `${url}/api/blogs`;
 export const debug = dev ? true : false;
